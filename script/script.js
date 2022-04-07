76d62b57960adcae00f84a7cdefaebca0c8cc6cd
Thallys <thalys_cezar@hotmail.com>
2022-04-07 09:11:39 -0300

    refactor: configurando script part5

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
+//colcando todos os valores recebidos em um objeto, podendo ser literario, com class ou com função.
 class Despesas {
-    constructor(ano, mes, dia, tipo, descricao, valor){
+    constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano;
         this.mes = mes;
         this.dia = dia;
@@ -9,18 +10,21 @@ class Despesas {
     }
 }
 
+//lógica dinâmica para o ID
 class Bd {
-    constructor(){
+    constructor() {
         let id = localStorage.getItem('id');
 
-        if(id === null) {
+        if (id === null) {
             localStorage.setItem('id', 0);
         }
     }
-    getProximoId(){
+    //pegando, sempre, o próximo ID, para, dessa forma, não sobrepor o ID anterior.
+    getProximoId() {
         let proximoId = localStorage.getItem('id');
         return parseInt(proximoId) + 1;
     }
+    //lógica para o id dinâmico e convertendo o objeto literário em JSON
     gravar(d) {
         //
         let id = this.getProximoId();
@@ -33,8 +37,8 @@ class Bd {
 
 let bd = new Bd();
 
-
-function cadastrarDespesas()  {
+//pegando os respectivos valores, para, dessa forma, ficar armazenado
+function cadastrarDespesas() {
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
     let dia = document.getElementById('dia');
@@ -43,13 +47,16 @@ function cadastrarDespesas()  {
     let valor = document.getElementById('valor');
 
     let despesas = new Despesas(
-        ano.value, 
-        mes.value, 
-        dia.value, 
-        tipo.value, 
-        descricao.value, 
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
         valor.value
-        );
+    );
 
     bd.gravar(despesas);
 }
+
+//foi introduzido, dessa forma, BD, o índice dinâmico
+//objeto BD é mais para controlar a lógica de acrescentar 1 no id, para, assim, não ficar sobrepondo o id anterior, a informação anterior.
