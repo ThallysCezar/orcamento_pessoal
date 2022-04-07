03d2f737ffbae30774a5042e1316664728a86ec1
Thallys <thalys_cezar@hotmail.com>
2022-04-07 13:29:47 -0300

    refactor: configurando script part20

--- script/script.js
+++ script/script.js
@@ -59,7 +59,7 @@ class Bd {
             if(despesa === null){
                 continue
             }
-
+            despesa.id = i;
             despesas.push(despesa);
         }
         return despesas
@@ -103,6 +103,10 @@ class Bd {
        }
 
        return despesasFiltradas;
+    }
+
+    remover(id) {
+        localStorage.removeItem(id);
     }
 }
 
@@ -191,6 +195,25 @@ function carregarListaDespesas(despesas = Array(), filtro = false){
         linha.insertCell(1).innerHTML = d.tipo;
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
+
+        //criar botao
+        let btn = document.createElement("button");
+        btn.className = 'btn btn-primary btn-danger';
+        btn.innerHTML = '<i class="fas fa-times"></i>';
+        btn.id = `id_despesa_${d.id}`;
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesa_', '');
+
+            alert(id);
+
+            bd.remover(id);
+
+            window.location.reload();
+        };
+        linha.insertCell(4).append(btn);
+
+
+
         console.log(d);
     });
 
