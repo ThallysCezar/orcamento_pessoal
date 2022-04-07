125a9a9d08c7c59b0fdd13d511595439e50c5991
Thallys <thalys_cezar@hotmail.com>
2022-04-07 08:22:16 -0300

    refactor: configurando script part1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+class Despesas {
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
+
+
+
 function cadastrarDespesas()  {
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
@@ -6,6 +19,14 @@ function cadastrarDespesas()  {
     let descricao = document.getElementById('descricao');
     let valor = document.getElementById('valor');
 
+    let despesas = new Despesas(
+        ano.value, 
+        mes.value, 
+        dia.value, 
+        tipo.value, 
+        descricao.value, 
+        valor.value
+        );
 
-
+    console.log(despesas);
 }
