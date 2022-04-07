2d28bd34e1931bee70c32f9c00f641f5d8c41dcc
Thallys <thalys_cezar@hotmail.com>
2022-04-07 08:15:21 -0300

    refactor: configurando script

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 function cadastrarDespesas()  {
-    alert("teste");
+    let ano = document.getElementById('ano');
+    let mes = document.getElementById('mes');
+    let dia = document.getElementById('dia');
+    let tipo = document.getElementById('tipo');
+    let descricao = document.getElementById('descricao');
+    let valor = document.getElementById('valor');
+
+
+
 }
